23406c684c1d222d046eb14fc15f375cd75cc7ae
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-18 13:23:11 +0530

    Made programs to show the use of typecasting

---
+++ Chapter 2 - Variables and datatypes/To Learn/07_typecasting_in_python.py
@@ -0,0 +1,8 @@
+a = "100"
+print(
+    "Here a is of ",
+    type(a),
+    "type \nbut we have to change it to int type so that we can perform the below operations",
+)
+print("\nChanging the type of a to [int]......")
+print("The value of a+1 is ", (int(a) + 1))

6bf9a722e0df5077884c7b157be8df33da72219b
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-18 10:36:27 +0530

    Added a program which prints poem twinkle twinkle little star

---
+++ Chapter 1 - Modules , Comments and Pips/To Practice/01_twinkle_twinkle_little_star.py
@@ -0,0 +1,26 @@
+print(
+"""Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are\n
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are\n
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are\n
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are"""
+)

4f5db0cf7be6bc2b7172e7758273dca94e37818d
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-18 13:35:39 +0530

    Made a program to show the working of input function

---
+++ Chapter 2 - Variables and datatypes/To Learn/08_take_input.py
@@ -0,0 +1,15 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+mother_name = input("Enter your mother's name : ")
+father_name = input("Enter your father's name : ")
+print(
+    "Hello ",
+    name,
+    " !!\nYour age is ",
+    age,
+    "\nand your Father's name is ->",
+    father_name,
+    "\nand Mother's name is ->",
+    mother_name,
+    " respectively",
+)

682e3191039ad29e4d50a22ef328b7dd993bce66
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-18 10:30:44 +0530

    Made program to show types of commenting in python

---
+++ Chapter 1 - Modules , Comments and Pips/02_line_commenting.py
@@ -0,0 +1,5 @@
+# Single line commenting shown below
+# My name is Biswajit Mishra.  --> As you can see that when we use the hash symbol then the line don't excute
+
+"""Multi-Line commenting shown here"""
+"""If we do like this then we can comment out multiple lines if needed"""

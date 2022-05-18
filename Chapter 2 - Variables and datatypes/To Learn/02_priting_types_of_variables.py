92f22ec847e025e3861a52339c0998cf01223c8a
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-18 11:59:57 +0530

    Made a program to print which types of data type is what

---
+++ Chapter 2 - Variables and datatypes/To Learn/02_priting_types_of_variables.py
@@ -0,0 +1,14 @@
+a = "Biswajit"  # double quote
+b = '''"Biswajit is a bad boy"'''  # triple quote
+c = '"Biswajit"'  # single quote
+d = 45  # integer
+e = 69.69  # float
+f = True
+g = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))

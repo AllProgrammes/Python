9c981565543399c3f1c25a7bef469bf1a04995cc
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-18 11:58:14 +0530

    Made a program to show types of data types

---
+++ Chapter 2 - Variables and datatypes/To Learn/01_variables.py
@@ -0,0 +1,14 @@
+a = "Biswajit"  # double quote
+b = '''"Biswajit is a bad boy"'''  # triple quote
+c = '"Biswajit"'  # single quote
+d = 45  # integer
+e = 69.69  # float
+f = True
+g = None
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)

5652d10ea8a17129e5c181b59440528397ff46d0
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 20:12:58 +0530

    Take favourite languages of 4 friends in a dictionary and print

---
+++ Chapter 5 - Dictionary and Sets/To Practice/04_friends_dict.py
@@ -0,0 +1,10 @@
+fav_lang = {}
+a = input("Enter your language Shreya  : ")
+b = input("Enter your language Sujal   : ")
+c = input("Enter your language Anubhav : ")
+d = input("Enter your language Akash   : ")
+fav_lang["Shreya"] = a
+fav_lang["Sujal"] = b
+fav_lang["Anubhav"] = c
+fav_lang["Akash"] = d
+print(fav_lang)

70e800c60eb897cf09f9f455742382c130cf503e
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 15:21:11 +0530

    Made a program to take 7 fruits name from the user and store them in a list

---
+++ Chapter 4 - Lists and Tuples/To Practice/01_store_fruits.py
@@ -0,0 +1,9 @@
+fruit1 = input("Enter the name of the fruit 1 : ")
+fruit2 = input("Enter the name of the fruit 2 : ")
+fruit3 = input("Enter the name of the fruit 3 : ")
+fruit4 = input("Enter the name of the fruit 4 : ")
+fruit5 = input("Enter the name of the fruit 5 : ")
+fruit6 = input("Enter the name of the fruit 6 : ")
+fruit7 = input("Enter the name of the fruit 7 : ")
+fruits_list = [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7]
+print(fruits_list)

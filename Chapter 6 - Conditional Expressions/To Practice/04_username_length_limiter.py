0cbe1ddff4933747449725f977d22f526909a0be
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 22:40:23 +0530

    Made a username detecter program

---
+++ Chapter 6 - Conditional Expressions/To Practice/04_username_length_limiter.py
@@ -0,0 +1,5 @@
+username = input("Enter your username here : ")
+if len(username) > 10:
+    print("Please enter username with length less than 10")
+else:
+    print("Thanks for using our service")

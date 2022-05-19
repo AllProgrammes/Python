d2257ca763649d101645b61e50ebf3aae2f6fbea
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 15:24:55 +0530

    Made a program to take marks of 6 students and sort them

---
+++ Chapter 4 - Lists and Tuples/To Practice/02_marks_of_students.py
@@ -0,0 +1,9 @@
+student1 = input("Enter the marks of student 1 : ")
+student2 = input("Enter the marks of student 2 : ")
+student3 = input("Enter the marks of student 3 : ")
+student4 = input("Enter the marks of student 4 : ")
+student5 = input("Enter the marks of student 5 : ")
+student6 = input("Enter the marks of student 6 : ")
+student_marks = [student1, student2, student3, student4, student5, student6]
+student_marks.sort()
+print(student_marks)

ee10aba92e7c9c7bbdf41c0a91b64b2c020f3aa5
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 19:58:03 +0530

    Added a iq check program for set

---
+++ Chapter 5 - Dictionary and Sets/To Practice/03_quick_test.py
@@ -0,0 +1,5 @@
+iq_test=set()
+iq_test.add(20)
+iq_test.add(20.0)
+iq_test.add("20")
+print("What will be the length of set (iq_test) ?\nAns :",len(iq_test),"because 20 and 20.0 is same for python but but but if we make it to 20.02 or 20.01 then it will count that as seperate value")

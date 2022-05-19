e35ae58dc3a3d510a74941442a20a7f7a7b6ab8f
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 19:51:35 +0530

    Take 6 numbers from the user and print the unique ones only

---
+++ Chapter 5 - Dictionary and Sets/To Practice/02_unique_numbers.py
@@ -0,0 +1,8 @@
+num1 = input("Enter num1 : ")
+num2 = input("Enter num2 : ")
+num3 = input("Enter num3 : ")
+num4 = input("Enter num4 : ")
+num5 = input("Enter num5 : ")
+num6 = input("Enter num6 : ")
+number = {num1, num2, num3, num4, num5, num6}
+print("All unique numbers are", number)

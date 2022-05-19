29660e3839a067c68d4b889bac8b2fbe817c007d
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 11:07:30 +0530

    Added a program to show how to do slicing in string

---
+++ Chapter 3 - Strings/To Learn/02_slicing.py
@@ -0,0 +1,7 @@
+name = "Biswajit"
+
+print(name[0])  # single slicing
+print(name[0:8])  # mutliple slicing
+print(name[:8])  # mutliple slicing in other form and is sama as name[0:8]
+print(name[0:])  # mutliple slicing again in other form and is sama as name[0:8]
+print(name[-8:-1])  # mutliple slicing again in other form and is sama as name[1:8]

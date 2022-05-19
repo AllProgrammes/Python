fa23a881d7ae6b90da287b9b7fbcce642775f06e
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 22:11:21 +0530

    Print the greatest among 4 numbers

---
+++ Chapter 6 - Conditional Expressions/To Practice/01_greatest.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter num1 : "))
+num2=int(input("Enter num2 : "))
+num3=int(input("Enter num3 : "))
+num4=int(input("Enter num4 : "))
+if num1>num2 and num1>num3 and num1>num4:
+    print(num1,"is the greatest among all")
+elif num2>num1 and num2>num3 and num2>num4:
+    print(num2,"is the greatest among all")
+elif num3>num1 and num3>num2 and num3>num4:
+    print(num3,"is the greatest among all")
+elif num4>num1 and num4>num2 and num4>num3:
+    print(num4,"is the greatest among all")    

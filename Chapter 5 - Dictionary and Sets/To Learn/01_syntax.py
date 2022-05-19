d959a91ad3572c6757c946f26b7c08b29763f659
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 16:10:57 +0530

    Made dictionary based on my own life

---
+++ Chapter 5 - Dictionary and Sets/To Learn/01_syntax.py
@@ -0,0 +1,18 @@
+#------------SIMPLE DICTIONARY---------------#
+myDict = {"Shreya": "My Girlfriend", "Love": "Shreya"}
+print(myDict["Love"])
+print(myDict["Shreya"])
+
+#------------NESTED DICTIONARY---------------#
+mylife={
+    "Life":
+        {
+         "Goal":"BCA",
+         
+         "Shreya":
+             {
+                 "Partner":"Future Wife"
+             }
+        }
+    }
+print(mylife["Life"]["Shreya"]["Partner"])

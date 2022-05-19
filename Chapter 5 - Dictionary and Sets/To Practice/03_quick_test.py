6500ae51c85f10dee6d4fea571f2b1d63c9dbe44
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 20:18:06 +0530

    Added ome more iq question

--- Chapter 5 - Dictionary and Sets/To Practice/03_quick_test.py
+++ Chapter 5 - Dictionary and Sets/To Practice/03_quick_test.py
@@ -1,5 +1,13 @@
-iq_test=set()
+iq_test = set()
 iq_test.add(20)
 iq_test.add(20.0)
 iq_test.add("20")
-print("What will be the length of set (iq_test) ?\nAns :",len(iq_test),"because 20 and 20.0 is same for python but but but if we make it to 20.02 or 20.01 then it will count that as seperate value")
+print(
+    "What will be the length of set (iq_test) ?\nAns :",
+    len(iq_test),
+    "because 20 and 20.0 is same for python but but but if we make it to 20.02 or 20.01 then it will count that as seperate value",
+)
+
+print(
+    "\nCan we change list inside a set ?\nAns : No,because :-\n   1. We cannot enter a list inside a set\n   2. We cannot change any value inside a set as it is rigid in nature"
+)

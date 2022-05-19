cb917dc4da54aeeba4d387e140a7ba6400993111
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 12:23:58 +0530

    Made a program to show different types of lists methods

---
+++ Chapter 4 - Lists and Tuples/To Learn/02_lists_methods.py
@@ -0,0 +1,13 @@
+list1 = [1, 5, 6, 67, 34, 4]
+list1.sort()  # sorts the list from low to high
+print(list1)
+list1.reverse()  # reverse the list
+print(list1)
+list1.append(3)  # adds the given value at the end of the list
+print(list1)
+list1.insert(0, 3)  # inserts the value 3 in idex 0
+print(list1)
+list1.pop(0)  # removes the vakue at index 0
+print(list1)
+list1.remove(3)  # searchs and removes the value 3
+print(list1)

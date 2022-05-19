3bc8134a9d1a7e89e3f2c74ff249358f8770e508
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-19 22:44:34 +0530

    Made a program to check if the user is my friend or not

---
+++ Chapter 6 - Conditional Expressions/To Practice/05_list.py
@@ -0,0 +1,6 @@
+name_list=["shreya","saad","sujal","anubhav"]
+check=input("Enter your name : ")
+if check in name_list:
+    print("You are Biswajit's friend")
+else:
+    print("Biswajit don't know you")

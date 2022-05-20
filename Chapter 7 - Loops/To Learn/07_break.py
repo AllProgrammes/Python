21a485f2e3247750810fca8a61cc265a0fc0c2ac
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-20 12:27:08 +0530

    Added programs to show the use of break and continue statements

---
+++ Chapter 7 - Loops/To Learn/07_break.py
@@ -0,0 +1,4 @@
+for i in range(8):
+    print(i)
+    if i==5:
+        break
---
+++ Chapter 7 - Loops/To Learn/08_continue.py
@@ -0,0 +1,11 @@
+for i in range(6):
+    if i == 4:  # don't see whats under it and skip it
+        continue
+    print("DJ RAJU test", i)
+
+# Output:-
+# DJ RAJU test 0
+# DJ RAJU test 1
+# DJ RAJU test 2
+# DJ RAJU test 3
+# DJ RAJU test 5

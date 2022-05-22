d20a2dfba51e720dfdcf3819f40183269a2f09f7
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-22 21:34:26 +0530

    Started OOPS->Class

---
+++ Chapter 10 - OOPS/To Learn/01_oops_railway_form.py
@@ -0,0 +1,10 @@
+class railwayform:
+    def printdata(self):
+        print(f"Name is {self.name}")
+        print(f"Train is {self.train}")
+
+
+biswajittrain = railwayform()
+biswajittrain.name = "Biswajit Mishra"
+biswajittrain.train = "COSMOS_SPECIAL"
+biswajittrain.printdata()

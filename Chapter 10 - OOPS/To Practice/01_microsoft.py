808c9fc4eebd992e69f99cbbd9be16d273983723
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-23 12:48:58 +0530

    Made a program to take employee details and print it

---
+++ Chapter 10 - OOPS/To Practice/01_microsoft.py
@@ -0,0 +1,12 @@
+class Programmers:
+    company = "Microsoft"
+
+    def __init__(self, name, code, salary):
+        print(
+            f"Company Name = {self.company}\nEmployee Name = {name}\nEmployee code = {code}\nSalary = {salary}$\n"
+        )
+
+
+biswajit = Programmers("Biswajit", "101", 69)
+Shreya = Programmers("Shreya", "102", 96)
+Sujal = Programmers("Sujal", "103", 19)

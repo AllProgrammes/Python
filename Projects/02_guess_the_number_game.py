49c75ce5bb8fb1dd1939ac8e819bd205c184cc7c
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-06-08 11:02:54 +0530

    Modified program to not crash when user enters anything other than number

--- Projects/02_guess_the_number_game.py
+++ Projects/02_guess_the_number_game.py
@@ -1,3 +1,4 @@
+from logging import exception
 import random
 
 # Welcome part of this game
@@ -17,20 +18,25 @@ computer = random.randint(1, 10)
 
 # Working of the game
 while count <= 3:
-    human = int(input("\nEnter your guessed number here : "))
-    if human == computer and count == 1:
-        print("Dam !! You guessed it in first attempt")
-        exit(0)
-    elif human == computer and count > 1:
-        print(f"You guessed it in {count} attempts")
-        exit(0)
-    elif human != computer:
-        if count == 3:
-            print(f"\nYou have lost the game\nThe correct number was {computer}")
-        else:
-            print(f"\nOops You are wrong , You have {3-count} attempts left")
-            if human > computer:
-                print("Hint : The number is less than what you have entered")
-            elif human < computer:
-                print("Hint : The number is more than what you have entered")
-    count += 1
+    human = input("\nEnter your guessed number here : ")
+    try:  # this will try changing tha data type to int
+        human = int(human)
+        if human == computer and count == 1:
+            print("Dam !! You guessed it in first attempt")
+            exit(0)
+        elif human == computer and count > 1:
+            print(f"You guessed it in {count} attempts")
+            exit(0)
+        elif human != computer:
+            if count == 3:
+                print(f"\nYou have lost the game\nThe correct number was {computer}")
+            else:
+                print(f"\nOops You are wrong , You have {3-count} attempts left")
+                if human > computer:
+                    print("Hint : The number is less than what you have entered")
+                elif human < computer:
+                    print("Hint : The number is more than what you have entered")
+        count += 1
+    except:  # if the data conversion fails somehow then the user will get the below message and the program will continue
+        print(f"You have entered {type(human)} value")
+        continue

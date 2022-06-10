1a02287c9b4a02c22f3cba721ece3453ba1f84df
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-06-10 12:52:43 +0530

    Made a program to show how (enumerate) works

---
+++ Chapter 12 - Error Handling/To Learn/09_enumerate.py
@@ -0,0 +1,9 @@
+shreya = ["Biswajit", "NEET", "B Sc", "Amity", "Job", "Dentist"]
+# index = 0
+# for item in shreya:
+#     print("No.", index, item)
+#     index += 1
+
+# This can be done as -->
+for index, item in enumerate(shreya):
+    print("No.", index, item)

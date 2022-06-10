bdc61e079bfe14497786eb0ee2a46156d53fcaee
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-06-10 20:44:09 +0530

    Make a program to print 3rd,4th & 7th element of a list using enumerate fx

---
+++ Chapter 12 - Error Handling/To Practice/02_print_elements_using_enumerate.py
@@ -0,0 +1,13 @@
+games = [
+    "Football",
+    "Chess",
+    "Ludo",
+    "Hockey",
+    "cricket",
+    "kho-kho",
+    "tennis",
+    "carrom",
+]
+for index, item in enumerate(games):
+    if index == 3 or index == 5 or index == 7:
+        print(f"SNo.{index}\t\t{item}")

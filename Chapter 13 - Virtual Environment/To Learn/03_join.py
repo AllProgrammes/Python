2017330e36b585e5bad2abcd364bdb9fe683365f
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-06-13 11:20:48 +0530

    Learned the use of join function

---
+++ Chapter 13 - Virtual Environment/To Learn/03_join.py
@@ -0,0 +1,12 @@
+some_stuff_that_i_have = [
+    "laptop",
+    "mobile",
+    "earphones",
+    "speaker",
+    "keyboard",
+    "mouse",
+]  # this is a list
+print_above_stuff = "\n".join(some_stuff_that_i_have)  # this is a string
+print(f"Some stuffs that I have are shown below :-\n{print_above_stuff}")
+# Q. So what this this join fx do?
+# Ans : It takes list as an argument and convert it into string by inserting whatever u have given like in this case i have given [\n] to insert

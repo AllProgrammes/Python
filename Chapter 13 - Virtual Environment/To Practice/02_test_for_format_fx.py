851eaebbb6a6f2e434aa9ddb5b6534bb2fd5840c
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-06-13 20:04:35 +0530

    Display the given information using format fx

---
+++ Chapter 13 - Virtual Environment/To Practice/02_test_for_format_fx.py
@@ -0,0 +1,7 @@
+name = input("Enter your name : ")
+marks = input("Enter your marks : ")
+mobile_number = input("Enter your mobile number : ")
+display = "The name of the student is {},his marks is {} and phone number is {}".format(
+    name, marks, mobile_number
+)
+print(display)

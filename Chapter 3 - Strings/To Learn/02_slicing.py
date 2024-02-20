2b294cad1ac8c085fb0a7028e494f064f5ee9656
Biswajit Mishra <mishrabiswajit660@gmail.com>
2024-02-20 07:14:43 +0000

    Added more ways of slicing

--- Chapter 3 - Strings/To Learn/02_slicing.py
+++ Chapter 3 - Strings/To Learn/02_slicing.py
@@ -5,3 +5,4 @@ print(name[0:8])  # mutliple slicing
 print(name[:8])  # mutliple slicing in other form and is same as name[0:8]
 print(name[0:])  # mutliple slicing again in other form and is same as name[0:8]
 print(name[-8:-1])  # mutliple slicing again in other form and is same as name[1:8]
+print(name[::-1]) # this will reverse the string as -1 here indicates the printing the string in reverse order and if we do it 1 then it will work same as any other mentioned above
